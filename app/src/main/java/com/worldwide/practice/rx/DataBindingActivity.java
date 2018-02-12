f2dae3a3dce6513468a51da6699a81d860e3202b
Anand <Anandpurushottam7@gmail.com>
2018-02-12 18:37:23 +0530

    Data binding  and buffer button click

--- app/src/main/java/com/worldwide/practice/rx/DataBindingActivity.java
+++ app/src/main/java/com/worldwide/practice/rx/DataBindingActivity.java
@@ -6,23 +6,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.jakewharton.rxbinding2.widget.RxTextView;
+import butterknife.OnTextChanged;
 import com.worldwide.practice.R;
-import com.worldwide.practice.rx.Util;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
-import io.reactivex.BackpressureStrategy;
-import io.reactivex.Flowable;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.subscribers.DisposableSubscriber;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.subjects.BehaviorSubject;
 import timber.log.Timber;
-
-/**
- * Created by Anand on 2/12/2018.
- */
-
+import static android.text.TextUtils.isEmpty;
+/** Created by Anand on 2/12/2018. */
 public class DataBindingActivity extends AppCompatActivity {
 
     @BindView(R.id.tvResult)
@@ -37,6 +32,7 @@ public class DataBindingActivity extends AppCompatActivity {
     Unbinder unbinder;
 
     Disposable disposable;
+    BehaviorSubject<Float> resultEmitterSubject;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -44,40 +40,45 @@ public class DataBindingActivity extends AppCompatActivity {
         setContentView(R.layout.data_binding);
         unbinder = ButterKnife.bind(this);
 
-        Flowable<Integer> first = RxTextView.textChanges(etFirst).toFlowable(BackpressureStrategy.LATEST).map(v -> {
-            if (Util.isNotNullOrEmpty(v)) {
-                return Integer.parseInt(v.toString());
-            } else return 0;
-        });
-        Flowable<Integer> last = RxTextView.textChanges(etLast).toFlowable(BackpressureStrategy.LATEST).map(v -> {
-            if (Util.isNotNullOrEmpty(v)) {
-                return Integer.parseInt(v.toString());
-            } else return 0;
-        });
-        //Disposable
-        disposable = Flowable.combineLatest(first, last, (x, y) -> x + y)
-                .subscribeWith(new DisposableSubscriber<Integer>() {
-                    @Override
-                    public void onNext(Integer integer) {
-                        updateUi(String.valueOf(integer));
-                    }
+        resultEmitterSubject = BehaviorSubject.create();
+        disposable =
+                resultEmitterSubject.subscribeWith(
+                        new DisposableObserver<Float>() {
+                            @Override
+                            public void onNext(Float aFloat) {
+                                updateUi(String.valueOf(aFloat));
+                            }
 
-                    @Override
-                    public void onError(Throwable t) {
-                        Timber.e(t);
+                            @Override
+                            public void onError(Throwable e) {
+                                Timber.e(e);
+                            }
 
-                    }
+                            @Override
+                            public void onComplete() {
+                                Timber.d("Completed");
+                            }
+                        });
+    }
 
-                    @Override
-                    public void onComplete() {
-                        Timber.d("Completed");
-                    }
-                });
+    @OnTextChanged({R.id.etFirst, R.id.etLast})
+    public void onNumberChanged() {
+        float num1 = 0;
+        float num2 = 0;
 
+        if (!isEmpty(etFirst.getText().toString())) {
+            num1 = Float.parseFloat(etFirst.getText().toString());
+        }
+
+        if (!isEmpty(etLast.getText().toString())) {
+            num2 = Float.parseFloat(etLast.getText().toString());
+        }
+
+        resultEmitterSubject.onNext(num1 + num2);
     }
 
     private void updateUi(String sum) {
-        tvResult.setText("Sum: " + sum);
+        tvResult.setText("BehaviorSubject: " + sum);
     }
 
     @Override
@@ -85,6 +86,5 @@ public class DataBindingActivity extends AppCompatActivity {
         super.onDestroy();
         unbinder.unbind();
         disposable.dispose();
-
     }
 }
